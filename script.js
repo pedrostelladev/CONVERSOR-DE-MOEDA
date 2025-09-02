1a7a99c217d68d954b53da0062faad62f67ff133
pedrostelladev <pedrostelladev@gmail.com>
2025-09-01 21:29:15 -0300

    valores formatados

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const convertButton = document.querySelector('.convert-button')
 
 function convertValues(){
     const inputValue = document.querySelector('.input-value').value
-        
+    const currencyValueToConvert = document.querySelector('.currency-value-to-convert')
+    const currencyValueToConverted = document.querySelector('.currency-value-converted')
+
+
     const dolarToday = 5.0
 
     let result = inputValue/dolarToday
 
-    console.log(result)
+currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+    style:'currency',
+    currency:'BRL'
+}).format(inputValue)
+
+
+currencyValueToConverted.innerHTML = new Intl.NumberFormat('en-US', {
+    style:'currency',
+    currency:'USD'
+}).format(result)
+
+console.log(result)
 }
 
 convertButton.addEventListener('click', convertValues)
