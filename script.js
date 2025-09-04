94c60cb5c591b81597f9f2b8222374779f5a975d
pedrostelladev <pedrostelladev@gmail.com>
2025-09-03 21:48:29 -0300

    alterado para troca de bandeira e nome da moeda

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const convertButton = document.querySelector('.convert-button') 
+const currencySelect = document.querySelector('.currency-select')
+
+
 
 function convertValues(){
     const inputValue = document.querySelector('.input-value').value
@@ -6,22 +9,45 @@ function convertValues(){
     const currencyValueToConverted = document.querySelector('.currency-value-converted')
 
 
-    const dolarToday = 5.0
+if(currencySelect.value == 'Dolar'){
+currencyValueToConverted.innerHTML = new Intl.NumberFormat('en-US', {
+    style:'currency',
+    currency:'USD'
+}).format(inputValue/dolarToday)
+}
 
-    let result = inputValue/dolarToday
+if(currencySelect.value == 'Euro'){
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat('de-DE', {
+        style:'currency',
+        currency:'EUR'
+    }).format(inputValue/euroToday)
+}
+
 
 currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
     style:'currency',
     currency:'BRL'
 }).format(inputValue)
 
-
-currencyValueToConverted.innerHTML = new Intl.NumberFormat('en-US', {
-    style:'currency',
-    currency:'USD'
-}).format(result)
-
-console.log(result)
 }
 
+function changeCurrency()   {
+   const currencyName = document.getElementById('eua')
+   const currencyImage = document.querySelector('.bandeira')
+
+if(currencySelect.value == 'Dolar') {
+    currencyName.innerHTML = 'Dolar Americano'
+    currencyImage.src ='./assets/Dolar.png'
+}
+
+if(currencySelect.value == 'Euro') {
+    currencyName.innerHTML = 'Euro'
+    currencyImage.src = './assets/Euro.png'
+}
+    
+convertValues()
+
+}
+
+currencySelect.addEventListener('change', changeCurrency)
 convertButton.addEventListener('click', convertValues)
