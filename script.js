a5c981306f22a8b4ef8d936650f11eeab9216285
pedrostelladev <pedrostelladev@gmail.com>
2025-09-04 20:32:47 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const convertButton = document.querySelector('.convert-button') 
 const currencySelect = document.querySelector('.currency-select')
 
-
+let euroToday = 6.37
+let dolarToday = 5.24
+let libraToday = 7.31
+let bitcoinToday = 603690.75
 
 function convertValues(){
     const inputValue = document.querySelector('.input-value').value
     const currencyValueToConvert = document.querySelector('.currency-value-to-convert')
     const currencyValueToConverted = document.querySelector('.currency-value-converted')
+
 
 
 if(currencySelect.value == 'Dolar'){
@@ -21,6 +25,18 @@ if(currencySelect.value == 'Euro'){
         style:'currency',
         currency:'EUR'
     }).format(inputValue/euroToday)
+}
+
+if(currencySelect.value == 'Libra'){
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat('en-GB', {
+        style: 'currency',
+        currency: 'GBP'
+    }).format(inputValue/libraToday)
+}
+
+if (currencySelect.value == 'Bitcoin') {
+    currencyValueToConverted.innerHTML = 
+        (Number(inputValue) / bitcoinToday).toFixed(8) + 'BTC'
 }
 
 
@@ -45,6 +61,16 @@ if(currencySelect.value == 'Euro') {
     currencyImage.src = './assets/Euro.png'
 }
     
+if(currencySelect.value == 'Libra'){
+    currencyName.innerHTML = 'Libra'
+    currencyImage.src = './assets/Libra.png'
+}
+
+if(currencySelect.value == 'Bitcoin'){
+    currencyName.innerHTML = 'Bitcoin'
+    currencyImage.src = './assets/Bitcoin.png'
+}
+
 convertValues()
 }
 
